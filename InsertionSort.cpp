e4f4b3ec7f83882ee2988f77ec000706cd6f1d6b
wiratamasultan65-max <wiratamasultan65@gmail.com>
2026-03-12 18:46:34 +0800

    membuat fungsi input

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -8,3 +8,17 @@ int main()
 {
 
 }
+
+void input(){
+    while (true){
+        cout << "Masukan Jumlah Data Pada Array : ";
+        cin >> n;
+
+        if (n <= 20){
+            break;
+        }
+        else{
+            cout << "\nArray Tidak Boleh Lebih dari 20\n";
+        }
+    }
+}
