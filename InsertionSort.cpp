ef3c7298e89115639fca4de151bf83aeb599c642
wiratamasultan65-max <wiratamasultan65@gmail.com>
2026-03-12 18:59:21 +0800

    Membuat fungsi display()

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -56,3 +56,16 @@ void insertionsort()
     }
 }
 
+void display()
+{
+    cout << endl;
+    cout << "\n====================";
+    cout << "\nElement Array" << endl;
+    cout << "\n====================";
+
+    for (int j = 0; j < n; j++){
+        cout << arr[j] << endl;
+    }
+
+    cout << endl;
+}
